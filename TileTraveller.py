be84ed99a0138fd725246fc10e81760e38def7e8
spraet96 <j-olli@hotmail.com>
2020-09-17 09:26:29 +0000

    Ã

---
+++ TileTraveller.py
@@ -0,0 +1,3 @@
+#Leikmaður fær upp valmöguleika um átt sem hann getur farið í og hann þarf að reyna að velja rétta átt til að sigra.
+#Stundum er bara ein leið í boði en allt að 3 leiðir í einu geta verið í boði.
+
