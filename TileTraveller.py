d3b70a95848ddc17844b9481596fdc89d01b907d
spraet96 <j-olli@hotmail.com>
2020-09-17 09:51:44 +0000

    updated algorithm

--- TileTraveller.py
+++ TileTraveller.py
@@ -1,3 +1,3 @@
 #Leikmaður fær upp valmöguleika um átt sem hann getur farið í og hann þarf að reyna að velja rétta átt til að sigra.
 #Stundum er bara ein leið í boði en allt að 3 leiðir í einu geta verið í boði.
-
+#Ef leikmaður velur átt sem er ekki í boði, þá fær hann upp "Not a valid direction!"
